affb53b22bf18c4c4c2964e9612893523fd9b70e
deliaIulianaPetrovici <54249437+deliaIulianaPetrovici@users.noreply.github.com>
2020-03-08 15:29:08 +0100

    updated our application with reselect, replaced cart selector with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {toogleCartHidden} from '../../redux/cart/cart.actions';
 import {ReactComponent as ShoppingIcon} from '../../assets/shoppingCart.svg';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 
 import './cart-icon.styles.scss';
 
 
-const CartIcon=({toogleCartHidden})=>(
+const CartIcon=({toogleCartHidden,itemCount})=>(
     <div className='cart-icon' onClick={toogleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
 
     </div>
 );
@@ -19,4 +20,10 @@ const mapDispachToProps=dispatch=>({
     toogleCartHidden:()=>dispatch(toogleCartHidden())
 });
 
-export default connect(null,mapDispachToProps)(CartIcon);
+const mapStateToProps=({cart:{cartItems}})=>({
+    itemCount: cartItems.reduce((accumulatedQuantity, cartItem)=>accumulatedQuantity+cartItem.quantity,
+        0
+    )
+});
+
+export default connect(mapStateToProps,mapDispachToProps)(CartIcon);
