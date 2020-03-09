8a846f48126b2cdfe4327760eedb29db97c0ab6b
deliaIulianaPetrovici <54249437+deliaIulianaPetrovici@users.noreply.github.com>
2020-03-09 12:27:41 +0100

    added addItem and removeItem functionality to checkout-item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,4 +7,15 @@ export const toogleCartHidden=()=>({
 export const addItem=item=>({
     type:CartActionTypes.ADD_ITEM,
     payload:item
-})
+});
+
+
+export const removeItem=item=>({
+    type:CartActionTypes.REMOVE_ITEM,
+    payload:item
+});
+
+export const clearItemFromCart=item=>({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload:item
+});
