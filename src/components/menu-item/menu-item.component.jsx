c48b6afd4eb40ba9d19bf07a31408de917ca2d40
deliaIulianaPetrovici <54249437+deliaIulianaPetrovici@users.noreply.github.com>
2020-03-09 18:39:32 +0100

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -12,7 +12,7 @@ const MenuItem=({title, imageUrl,size, history,linkUrl,match})=>(
         
                 <div className='content'>
                     <h1 className='title'>{title.toUpperCase()}</h1>
-                    <spam className='subtitle'>SHOP NOW</spam>
+                    <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
 );
